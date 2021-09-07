229c614f3553b6f42e77f047be36f6c4e9b7ed64
unknown <zc_cjh@163.com>
2021-09-07 15:23:25 +0800

    code commit first

---
+++ Socket通信/UDP_Test/UDP_Test/UDPTest.h
@@ -0,0 +1,20 @@
+#ifndef _UDP_TEST_H_
+#define _UDP_TEST_H_
+
+
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#pragma comment(lib, "Ws2_32.lib")
+
+
+// 绑定IP地址和端口
+BOOL Bind(char *lpszIp, int iPort);
+// 数据发送
+void SendMsg(char *lpszText, char *lpszIp, int iPort);
+// 数据接收
+void RecvMsg();
+// 接收数据多线程
+UINT RecvThreadProc(LPVOID lpVoid);
+
+
+#endif
